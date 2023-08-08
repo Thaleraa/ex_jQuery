2b45deb627c81f76e415c9a5c1d52142feb36a06
Thales Campelo <thalesgcampelo@gmail.com>
2023-08-08 11:46:21 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $('form').on('submit', function(e){
+        e.preventDefault()
+        const novaTarefa = $('#tarefa').val()
+        const novoItem = $('<li style="display: flex-wrap"> </li>')
+
+        $(`<li>${novaTarefa}</li>`).appendTo(novoItem)
+        $(novoItem).appendTo('ul')
+        $('#tarefa').val('')
+
+        $('ul').click (function(){
+            $('li').css({"text-decoration":"line-through"})
+        })
+        })
+    })
+
